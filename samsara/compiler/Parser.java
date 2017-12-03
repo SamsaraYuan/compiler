c33043822a1116bc98ae170f5c05bb2265f24565
samsara <1064583007@qq.com>
2017-12-03 15:26:37 +0800

    V1.0: parse and lexer

---
+++ samsara/compiler/Parser.java
@@ -0,0 +1,103 @@
+import java.io.*;
+
+class Parser
+{
+	private static int lookahead;
+	/////////////////////////////////////////////////
+	public Parser() throws IOException
+	{
+		lookahead = System.in.read();
+	}
+	
+	public void expr() throws IOException
+	{
+		term();
+		while (true)
+		{
+			if (lookahead == '+')
+			{
+				match('+');
+				term();
+				System.out.write('+');
+			}
+			
+			else if (lookahead == '-')
+			{
+				match('-');
+				term();
+				System.out.write('-');
+			}
+			
+			else	return;
+		}
+	}
+	/////////////////////////////////////////////////
+	private void term() throws IOException
+	{
+		if (Character.isDigit((char)lookahead))
+		{
+			System.out.write((char)lookahead);
+			match(lookahead);
+		}
+		else throw new Error("syntax error");
+	}
+	
+	private void match(int t) throws IOException
+	{
+		if (lookahead == t)		lookahead = System.in.read();
+		else 					throw new Error("syntax error");
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ samsara/compiler/ParserTest.java
@@ -0,0 +1,63 @@
+import java.io.*;
+
+public class ParserTest 
+{
+	public static void main(String[] args) throws IOException
+	{
+		Parser parse = new Parser();
+		parse.expr();
+		System.out.write('\n');
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
